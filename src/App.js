f4b61b9a20cba6eeed47ec9935dddac0a5e02afd
Kalvium <program@kalvieducation.com>
2022-12-25 17:45:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+
+import React,{ useCallback, useEffect, useMemo, useState} from 'react';
 import './App.css';
 
+const LARGE_NUMBER = 1000000000;
+
 function App() {
+
+
+  const [value, setValue] = useState(0);
+  const [dark, setTheme] = useState(true);
+  const [themeName, setThemeName] = useState("dark");
+  const [currentList, setList] = useState([]);
+
+  // const delayFunction = ()=> {
+  //   console.log("Delay Function Ran")
+  //   for(let index=0; index<LARGE_NUMBER; index++){};
+  //   return value+2;
+  
+  // }
+
+  // const callBackFunction = ()=>{
+  //   return [value*3 ,value*4]
+  // }
+
+
+  const callBackFunction = useCallback(()=>{
+    return [value*3,value*4]
+  },[value])
+
+  const delayFunction = useMemo(()=> {
+    console.log("it ran")
+    for(let index=0; index<LARGE_NUMBER; index++){};
+    return value+2;
+  },[value])
+  
+
+  useEffect(()=>{
+    console.log("Callback Function was called")
+  },[callBackFunction])
+
+
+
+  useEffect(()=>{
+    if(dark){
+      setThemeName("dark")
+    }
+    else{
+      setThemeName("light")
+    }
+  },[dark])
+
+
+  const handleClick = ()=>{
+    setTheme(!dark);
+  }
+
+  const handleChangeValue = ()=>{
+    setValue(value+1);
+  }
+
+  const handleList = ()=>{
+    setList(callBackFunction);
+  }
+
+  const styleTheme = {
+   backgroundColor:dark ? "black":"#ccc7c7",
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="page" style={styleTheme}>
+      <button onClick={handleClick}>{themeName}</button>
+      <h1 >{value}</h1>
+      <button onClick={handleChangeValue}>Change Value</button>
+      <button onClick={handleList}>Show List</button>
+      <h2>{delayFunction}</h2>
+      <div>
+        {currentList.map((item,index)=>{
+          return <h2 key={index}>{item}</h2>
+        })}
+
+      </div>
     </div>
+    
   );
 }
 
